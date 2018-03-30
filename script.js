5345c02314b30f73ea662167e4d8e187f479c4ab
Nailan Ferreira <nailan4052@gmail.com>
2018-03-30 20:52:42 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+alert("Olá meu nome é IRock");
+
+var name = prompt("Qual o seu nome?");
+
+if(name){
+	alert("Estou muito feliz em conhece-lo " + name);
+}else{
+	alert("Queria muito conhece-lo");
+}
